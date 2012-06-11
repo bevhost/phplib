006dbf0058d2509cdf50f16316e70571d722e2f8
David Beveridge <dave@bevhost.com>
2012-06-12 08:14:02 +1000

    reduce sql queries from one per menu entry to one

--- local/phplib/menus.php
+++ local/phplib/menus.php
@@ -1,54 +1,73 @@
 <?php 
-global $widthTotal;
+global $q, $widthTotal, $menu;
 $widthTotal = 0;
 
-function menu($parent,$indent) {
-	global $widthTotal, $sess;
-	$db = new $_ENV["DatabaseClass"];
-	$sql = "select * from menu where parent=$parent order by position";
-	if (!$db->query($sql)) echo "Error(no rows) $sql";;
-        if ($parent) if ($db->num_rows()) {
-                $start_str = "\n$indent <ul>";
-                $end_str = "\n$indent </ul>\n$indent";
-        }
-	while ($db->next_record()) {
-		extract($db->Record);
-                $ok = false;
-                if ($view_requires) {
+if (substr($_SERVER["REQUEST_URI"],0,strlen($_SERVER["SCRIPT_NAME"]))==$_SERVER["SCRIPT_NAME"]) 
+     $ModReWritten=false;
+else $ModReWritten = true;
+
+$db = new $_ENV['DatabaseClass'];
+$sql = "SELECT * FROM menu";
+$db->query($sql);
+while ($db->next_record()) {
+        extract($db->Record);
+	$ok = false;
+        if ($view_requires) {
+               if ($perm) {
                         foreach(explode(",",$view_requires) as $need) {
-                                if ($sess->have_perm($need)) $ok = true;
+                                if ($perm->have_perm($need)) $ok = true;
                         }
-                } else $ok = true;
-                if ($ok) {
-                    if ($start_str) {
-			echo $start_str;
-			$start_str = "";
-                    }
-		    if ($target=="menu") $target = "menupage.php?MenuId=$id";
-		    if (!strpos($target,'://')) $target = "/$target";
-		    if ($target=="/ContentEditor.php") {
-			if ($_REQUEST["page"]) { $target .= "?page=".$_REQUEST["page"]; $skip=false; } else $skip=true;
-		    } else $skip=false;
-		    if (!$skip) {
-			echo "\n$indent  <li><a href='$target'";
-			if (!$parent) {
-				if ($_ENV["MenuMode"]=="horiz") echo " class='menu' style='width:".$width."px'";
-				$widthTotal += $width;
-			}
-			echo ">$title</a>";
-			menu($id,$indent."  ");
-			echo "</li>";
-		    }
-		}
+                }
+        } else $ok = true;
+        if ($ok) {
+		if ($target=="menu") $target = "menupage.php?MenuId=$id";
+		if (substr($target,0,4)<>"http") $target = "/$target";
+		$menu[$id]->target = $target;
+		$menu[$id]->title = $title;
+		$menu[$id]->width = $width;
+		$menu[$parent]->children[$position] = $id;
 	}
-	if (!$start_str) echo $end_str;
 }
 
-echo "<li><a href='/ContentEditor.php?".$_SERVER["QUERY_STRING"]."' id='NavHome'>&nbsp;</a></li>\n";
+function menu($parent,$indent) {
+	global $widthTotal, $menu;
+	if (!isset($menu[$parent]->children)) return;
+	foreach ($menu[$parent]->children as $sortorder => $id) {
+		$items[] = $sortorder;
+	}
+	sort($items);
+	if ($parent) echo "\n$indent <ul>";
+	foreach($items as $item) {
+		$id = $menu[$parent]->children[$item];
+		$target = $menu[$id]->target;
+		$title = $menu[$id]->title;
+		echo "\n$indent  <li><a href='$target'";
+		if (!$parent) {
+			echo " class='menu'";
+			$widthTotal += $menu[$id]->width;
+		}
+		echo ">$title</a>";
+		menu($id,$indent."  ");
+		echo "</li>";
+	}
+	if ($parent) echo "\n$indent </ul>\n$indent";
+}
+
 menu(0,"");
-$width = 919 - $widthTotal;
-if ($_ENV["MenuMode"]=="horiz") $style = " style='width:".$width."px'"; else $style="";
-echo "<li><a href='/MenuEditor.php' id='MenuEnd'$style>&nbsp;</a></li>\n";
+if (empty($GLOBALS["widemode"])) $screen = 919; else $screen=1519;
+$width = $screen - $widthTotal;
+if ($width>199) {
+	$fill = $width - 199;
+	echo '<li><a href="#" style="width:'.$fill.'px" class="menufill"></a></li>';
+	$width = 199;
+}
+
+/*
+if (isset($q)) {
+  if (empty($q->start_row)) { $q->start_row = 0; }
+  if ($q->start_row < 1) { $q->start_row = 0; }
+  if (empty($q->row_count)) { $q->row_count = 50; }
+}
+*/
 
 ?>
-
